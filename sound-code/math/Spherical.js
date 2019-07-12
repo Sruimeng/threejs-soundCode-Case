318fe0c7c3f9186c631311a3788af1a3d0323b1b
SuRuiMeng <934274351@qq.com>
2019-07-12 18:19:21 +0800

    1

--- sound-code/math/Spherical.js
+++ sound-code/math/Spherical.js
@@ -11,11 +11,11 @@ import { _Math } from './Math.js';
  */
 
 /**
- * @description 
+ * @description 球体坐标系作为一个球体的结构模型，对应的点在球上，phi的0在在球的正上方，theta的0在球的正面的中心
  * 
- * @param {any} radius 
- * @param {any} phi 
- * @param {any} theta 
+ * @param {any} radius 半径
+ * @param {any} phi 俯仰角
+ * @param {any} theta 方位角
  * @returns  
  */
 function Spherical( radius, phi, theta ) {
@@ -29,7 +29,7 @@ function Spherical( radius, phi, theta ) {
 }
 
 Object.assign( Spherical.prototype, {
-
+	//设置球体坐标系的数值
 	set: function ( radius, phi, theta ) {
 
 		this.radius = radius;
@@ -39,13 +39,13 @@ Object.assign( Spherical.prototype, {
 		return this;
 
 	},
-
+	//克隆方法
 	clone: function () {
 
 		return new this.constructor().copy( this );
 
 	},
-
+	//复制
 	copy: function ( other ) {
 
 		this.radius = other.radius;
@@ -55,7 +55,7 @@ Object.assign( Spherical.prototype, {
 		return this;
 
 	},
-
+	// 判断查询phi的精度
 	// restrict phi to be betwee EPS and PI-EPS
 	makeSafe: function () {
 
@@ -65,17 +65,17 @@ Object.assign( Spherical.prototype, {
 		return this;
 
 	},
-
+	//从一个三维坐标设置球体坐标系
 	setFromVector3: function ( v ) {
 
 		return this.setFromCartesianCoords( v.x, v.y, v.z );
 
 	},
-
+	//从一个三维坐标设置球体坐标系
 	setFromCartesianCoords: function ( x, y, z ) {
 
 		this.radius = Math.sqrt( x * x + y * y + z * z );
-
+		//如果半径为0的话，其他数据也为0
 		if ( this.radius === 0 ) {
 
 			this.theta = 0;
