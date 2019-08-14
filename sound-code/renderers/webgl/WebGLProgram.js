11ed0d0ed432bde5577dc48cda15fb75f7a4310f
SuRuiMeng <934274351@qq.com>
2019-08-14 18:36:35 +0800

    1

--- sound-code/renderers/webgl/WebGLProgram.js
+++ sound-code/renderers/webgl/WebGLProgram.js
@@ -20,7 +20,7 @@ function getEncodingComponents( encoding ) {
 		case RGBEEncoding:
 			return [ 'RGBE', '( value )' ];
 		case RGBM7Encoding:
-			return [ 'RGBM', '( value, 7.0 )' ];
+			return [ 'RGBM', '( value, 15.0 )' ];
 		case RGBM16Encoding:
 			return [ 'RGBM', '( value, 16.0 )' ];
 		case RGBDEncoding:
