184e53befaf18ec165c4eedec03522210c108f59
SuRuiMeng <934274351@qq.com>
2019-09-27 17:50:17 +0800

    1

--- sound-code/loaders/Cache.js
+++ sound-code/loaders/Cache.js
@@ -1,13 +1,18 @@
 /**
  * @author mrdoob / http://mrdoob.com/
  */
-
+/**
+ * 一个简单的缓存系统
+ * 要在所有使用FileLoader的加载器上启用缓存， 需设置
+ * THREE.Cache.enabled = true.
+ * 一个Map然后key是字符串，value为文件
+ */
 var Cache = {
 
 	enabled: false,
 
 	files: {},
-
+	// 使用key为引用文件增加一个缓存入口。如果该key已持有一个文件，则会被覆盖。
 	add: function ( key, file ) {
 
 		if ( this.enabled === false ) return;
