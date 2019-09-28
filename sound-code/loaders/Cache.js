7a97c747c774cfa4505665880fb828fd28751d62
SuRuiMeng <934274351@qq.com>
2019-09-28 18:04:36 +0800

    1

--- sound-code/loaders/Cache.js
+++ sound-code/loaders/Cache.js
@@ -22,7 +22,7 @@ var Cache = {
 		this.files[ key ] = file;
 
 	},
-
+	//获得该key的值。 如果该key不存在，则以undefined被返回。
 	get: function ( key ) {
 
 		if ( this.enabled === false ) return;
@@ -32,13 +32,13 @@ var Cache = {
 		return this.files[ key ];
 
 	},
-
+	//使用key来删除相应的缓存文件。
 	remove: function ( key ) {
 
 		delete this.files[ key ];
 
 	},
-
+	//清除所有缓存中的值。
 	clear: function () {
 
 		this.files = {};
